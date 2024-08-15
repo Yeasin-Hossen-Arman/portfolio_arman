4d078a1b4a5f135469ba19a2368194d93f6fb906
Yeasin-Hossen-Arman <yeasinhossenarman@gmail.com>
2024-08-16 01:13:16 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Nav from "./components/Nav"
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+        {/* <!-- component -->
+<style>
+  .bg-dots{
+    background-image: url(https://assets-global.website-files.com/5b5a66e9f3166b36708705fa/5dea7a12bb83ab1f13040de5_cx-dots.svg);
+    background-repeat: no-repeat
+  }
+</style> */}
+
+<div class="m-auto max-w-6xl p-12">
+   <div class="flex flex-col md:flex-row">
+      <div class="md:w-1/2 max-w-md flex flex-col justify-center">
+         <div class="md:text-5xl text-2xl uppercase font-black">Yeasin Hossen Arman</div>
+         <div class="text-xl mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+         <div class="my-5 h-16">
+            <div class="shadow-md font-medium py-2 px-4 text-yellow-100
+               cursor-pointer bg-yellow-600 hover:bg-yellow-500 rounded text-lg text-center w-48">Hire me</div>
+         </div>
+      </div>
+      <div class="flex md:justify-end w-full md:w-1/2 -mt-5 ">
+         <div class="bg-dots">
+            <div class="flex flex-col justify-center shadow-2xl max-w-md z-10 rounded-full mt-6 ml-4 ">
+               <img alt="card img" width="150px" height="20px" class="rounded-t ml-24" src="src\assets\file.png" /> 
+               <div class="text-2xl p-10 bg-white">I'm passionate with Python, Django, Flask, Machine learning, Deep learning, Computer vision and image processing</div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+    </div>
+  )
+}
+
+export default Hero
